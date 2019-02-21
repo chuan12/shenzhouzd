83e1892fdbe1e29e84c36cc15ef0bd7baabfbe76
chuan12 <38429025+chuan12@users.noreply.github.com>
2019-02-21 12:44:38 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var feed_data = {  
     "domain":  { "name": "当前域名", 
-                 "value": [ "WkRKd05Hd3lNbU5yTnpBeWVERXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "Wkhsd2VXcGxlWFkxWWpnNGFDNWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ==", "WkRaaVoydHBlbVl5ZDNObFp5NWpiRzkxWkdaeWIyNTBMbTVsZEE9PQ=="],
+                 "value": [ "WkRKd05Hd3lNbU5yTnpBeWVERXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRKMVl6aHZjSE5zWlhCNmFEQXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ==", "WkRKd05Hd3lNbU5yTnpBeWVERXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="],
                  "fq": "WkRNME1HTnBOalowZVdocWVqY3VZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="
                },
                
@@ -15,10 +15,10 @@ var feed_data = {
                  "value": [ "TVRFMExqSTBMamd3TGpFeE5RPT0=", "TVRFNExqRTJPQzR5TURndU1qSXc=", "TVRFMExqSTBMamd3TGpFeE5RPT0="]
                }, 
   "longacting": { "name": "longdomain",
-                 "value": [ "TWpjdU1UQTFMak01TGpVdw==", "TmpFdU1qTXdMalEzTGpFME9BPT0=", "WkRGdGNIWm9aalkzWm05clltMHVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="]
+                 "value": [ "TWpjdU1UQTFMak01TGpVdw==", "TmpFdU1qTXdMalEzTGpFME9BPT0=", "WkRKNGIydGxZVGMzTlhkNk5XRXVZMnh2ZFdSbWNtOXVkQzV1WlhRPQ=="]
                },
   "ogColl": { "name": "网门",
-                 "value": [ "ZEM1amJpOUZlbkZxVmxOTQ==", "ZEM1amJpOUZSMWhyWVhWVg==", "YUdsMlkzSm5hQzVoWkhONGJXeGlMbWR4THpZNA=="]
+                 "value": [ "ZEM1amJpOUZlbkZxT0VkUA==", "ZEM1amJpOUZSMWhyUlROdQ==", "WW5WdmIyTjZMbXh4Y0dGMWRpNWpaaTgyT0E9PQ=="]
                }, 
 
     "article": { "name": "最新文章", 
@@ -86,7 +86,7 @@ var feed_data = {
                  "url": "https://git.io/fxNjC",
                  "sha1": "0D35F7294986F2DB2FEE689AD470693791671551",
                  "sha512": "f6f49be558304cffe23a8d2efb154825e92de3d25a7142d9d092eda0fa1d09cee4284a8f7fe80d3abec34d119113b7f9a38d5f2a3c30ec26e5a1a249b10517b0",
-                 "time": "1550753010"
+                 "time": "1550753074"
                } 
      
  }     
